ec82d72383a62da40b83c5ec6fc1a608d0d124fb
yc18841 <y_chen_98@yahoo.com>
2025-02-17 17:37:36 -0500

    Initial commit

---
+++ src/main/java/com/example/springsecurityjwt/MyUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.springsecurityjwt;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// Create a user with username "foo" and password "foo"
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return new User("foo", encoder.encode("foo"), new ArrayList<>());
+	}
+
+}
